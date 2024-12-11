a51cb571bb7535415d9ac88923f44c5bbc28c9d5
harikagavini <harikagavini@gmail.com>
2024-12-10 19:12:29 -0800

    chore: shortening the menu...

--- ui/src/app/components/NavBar.js
+++ ui/src/app/components/NavBar.js
@@ -1,18 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = ({ options }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <nav className="navbar">
-      <div className="logo"><Link href="/">LifeLine</Link></div>
-      <ul className="nav-links">
-        {options.map((option, index) => (
-          <li key={index}><Link key={index} href={option.href}>{option.label}</Link></li>
-        ))}
-      </ul>
-      <div className="menu-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="logo">
+        <Link href="/">LifeLine</Link>
+      </div>
+      <div className="nav-links-dropdown">
+        <button className="dropdown-toggle" onClick={handleToggle}>
+          Menu
+        </button>
+        {toggle && (
+          <ul className="dropdown-menu">
+            {options.map((option, index) => (
+              <li key={index}>
+                <Link key={index} href={option.href}>
+                  {option.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </nav>
   );
