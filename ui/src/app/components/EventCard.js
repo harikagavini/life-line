3105c22c0b0bdad6788b2f0713a1e7f33f9b864d
harikagavini <harikagavini@gmail.com>
2024-12-11 16:24:59 -0500

    Event card draft

---
+++ ui/src/app/components/EventCard.js
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+const EventCard = ({ event, onEdit, onDelete, isEditable }) => {
+  const { name, eventDate, street, city, state, zip } = event;
+
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-date">
+          Date: {new Date(eventDate).toLocaleDateString()}
+        </p>
+        <p className="card-address">
+          {street}, {city}, {state}, {zip}
+        </p>
+        <div className="card-actions">
+          {isEditable && (
+            <button className="btn-edit" onClick={() => onEdit(event)}>
+              Edit
+            </button>
+          )}
+          {isEditable && (
+            <button className="btn-delete" onClick={() => onDelete(event.id)}>
+              Delete
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
