2e7f24d59536e619669cd4c9cf1eb1bea330ecb0
harikagavini <harikagavini@gmail.com>
2024-12-11 17:41:07 -0500

    wip: Event edit

--- ui/src/app/components/EventCard.js
+++ ui/src/app/components/EventCard.js
@@ -1,9 +1,27 @@
 "use client";
 
 import React from "react";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const EventCard = ({ event, onEdit, onDelete, isEditable }) => {
   const { name, eventDate, street, city, state, zip, branchId, eventId } = event;
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleEditClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+  
+  const handleSubmit = async (updatedEvent) => {
+    const success = await onEdit(updatedEvent);
+    if (success) {
+      handleCloseModal();
+    }
+  };
 
   return (
     <div className="card">
@@ -28,6 +46,52 @@ const EventCard = ({ event, onEdit, onDelete, isEditable }) => {
           )}
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const updatedEvent = {
+              ...event,
+              name: e.target.name.value,
+              eventDate: e.target.eventDate.value,
+              street: e.target.street.value,
+              city: e.target.city.value,
+              state: e.target.state.value,
+              zip: e.target.zip.value,
+            };
+            handleSubmit(updatedEvent);
+          }}
+        >
+          <h3>Edit Event</h3>
+          <div>
+            <label>Name:</label>
+            <input type="text" name="name" defaultValue={event.name} required />
+          </div>
+          <div>
+            <label>Date:</label>
+            <input type="date" name="eventDate" defaultValue={event.eventDate} required />
+          </div>
+          <div>
+            <label>Street:</label>
+            <input type="text" name="street" defaultValue={event.street} required />
+          </div>
+          <div>
+            <label>City:</label>
+            <input type="text" name="city" defaultValue={event.city} required />
+          </div>
+          <div>
+            <label>State:</label>
+            <input type="text" name="state" defaultValue={event.state} required />
+          </div>
+          <div>
+            <label>Zip:</label>
+            <input type="text" name="zip" defaultValue={event.zip} required />
+          </div>
+          <button type="submit" className="btn-edit">
+            Save
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 };
