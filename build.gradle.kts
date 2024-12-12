9466f52c4310da822d2b4d0ea6f577fe523e1f91
harikagavini <harikagavini@gmail.com>
2024-12-12 00:39:48 -0500

    gradle-integration-complete

---
+++ build.gradle.kts
@@ -0,0 +1,3 @@
+tasks.register("startUI") {
+    dependsOn(":ui:dev")
+}
--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 
 const dbConfig = {
   host: 'localhost',
-  user: 'root',
+  user: 'test',
   password: 'Qwerty12345',
   database: 'lifeline'
 };
